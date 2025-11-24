0178ccdecdf992b5d8886c6106af6439a2564056
YoonGangHoon <yghun021007@naver.com>
2025-11-24 14:03:48 +0900

    feat: 회원가입 DTO 및 화면

---
+++ src/main/java/knu/atoz/member/dto/SignupRequestDto.java
@@ -0,0 +1,34 @@
+package knu.atoz.member.dto;
+
+import knu.atoz.member.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class SignupRequestDto {
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private String name;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthDate;
+
+    public Member toEntity() {
+        return new Member(
+                this.email,
+                this.password,
+                this.name,
+                this.birthDate,
+                LocalDateTime.now()
+        );
+    }
+}
